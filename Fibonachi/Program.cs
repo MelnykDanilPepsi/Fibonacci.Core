ba0f6bacefefd52a16e8c384a245cb823538d4a7
MelnykDanilPepsi <da.melnik2011@gmail.com>
2022-01-31 15:42:28 +0200

    first commit

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,44 @@
+﻿UInt64 num;
+for(; ;)
+{
+    try
+    {
+        Console.Clear();
+        Console.WriteLine("Enter num,please   | 0 -exit");
+        num = UInt64.Parse(Console.ReadLine());
+
+    }
+    catch { continue; }
+    if (num == 0) break;
+
+    Thread tr = new Thread(() =>
+    Fibonacci(num)
+    );
+    tr.Start();
+    Console.ReadKey();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+void Fibonacci(UInt64 n)
+{
+    if (n == 0 || n == 1)
+    {
+        Console.WriteLine("Error num");
+        return;
+    }
+    for(UInt64 i = 0; i < n;i++)
+    {
+        n += (n - 1) + (n - 2);
+    }
+    Console.WriteLine("Fibonacci num:"+ n);
+}
